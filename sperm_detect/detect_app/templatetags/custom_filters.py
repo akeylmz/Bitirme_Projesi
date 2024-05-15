79253a09995a43d7e30362d04f03fb4ee0383a16
Ahmet Kemal Ylmaz <97607813+akeylmz@users.noreply.github.com>
2024-05-15 10:04:00 +0300

    click canvas

--- sperm_detect/detect_app/templatetags/custom_filters.py
+++ sperm_detect/detect_app/templatetags/custom_filters.py
@@ -13,3 +13,20 @@ def remove_extension(value):
     base_name = os.path.basename(value)
     name_only, _ = os.path.splitext(base_name)
     return name_only
+
+
+@register.filter
+def calc_x(value, arg_w):
+    return (value-arg_w/2) * 1920
+
+@register.filter
+def calc_y(value, arg_h):
+    return (value-arg_h/2) * 1080
+
+@register.filter
+def calc_w(value):
+    return value * 1920
+
+@register.filter
+def calc_h(value):
+    return value * 1080
